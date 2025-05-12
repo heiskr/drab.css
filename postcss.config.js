3314633e371b60a79828ca301ee04e4741607c8c
Kevin Heis <heiskr@users.noreply.github.com>
2025-05-11 20:49:19 -0700

    update packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   plugins: [
-    require('@csstools/postcss-oklab-function')({
-      subFeatures: { displayP3: false },
-    }),
     require('cssnano')({
       preset: 'advanced',
     }),
